180b35d5e7cd77bf281014467213f9db2ae8ded0
Simon11287 <71406841+Simon11287@users.noreply.github.com>
2020-10-08 12:55:55 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -14,19 +14,18 @@ Example:
 1 1 2 3 5 ...
 """
 
-nterms = int(input("How many terms? "))
-n1, n2 = 0, 1
-count = 0
 
-# check if the number of terms is valid
-if nterms <= 0:
-   print("Please enter a positive integer")
-elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
-   print(n1)
-else:
-   print("Fibonacci sequence:")
-   while count < nterms:
+numberofterms = int(input("how many terms? "))
+count = 0
+n1, n2 = 0, 1
+
+if numberofterms<=0 :
+    print("enter a integer")
+elif numberofterms == 1 :
+    print("Print the sequence up to" ,numberofterms,":")
+else : 
+    print("Fibonacci sequence")
+while count < numberofterms:
        print(n1)
        nth = n1 + n2
        # update values
